5c5fe2ec5632dc0a8248745a3e3f9375dc12284a
Shruti Jain <jainshruti408@gmail.com>
2025-08-18 15:53:19 +0530

    [frontend]-added OrderForm component and send order-data to backend

--- orders-ui/src/pages/CreateOrder.jsx
+++ orders-ui/src/pages/CreateOrder.jsx
@@ -1,9 +1,16 @@
 import Header from "../components/Header";
+import OrderForm from '../components/OrderForm';
 
 function CreateOrder() {
+
     return (
         <>
             <Header />
+            <div className="container col-md-8 col-lg-8 mt-4">
+                <h3 className="p-2" >Create New Order </h3>
+                <OrderForm />
+            </div>
+
         </>
 
     )
