6d4bb883a3e2b481a7effdc165c839a921d223d1
Shruti Jain <jainshruti408@gmail.com>
2025-08-18 15:34:32 +0530

    [frontend]-added OrdersTable and fetch orders

--- orders-ui/src/App.jsx
+++ orders-ui/src/App.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import CreateOrder from './pages/CreateOrder';
 import OrderPage from './pages/OrderPage';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/create" element={<CreateOrder />} />
         <Route path="/orders/:id" element={<OrderPage />} />
       </Routes>
+      <ToastContainer/>
+
        
     </>
   )
--- orders-ui/src/pages/Dashboard.jsx
+++ orders-ui/src/pages/Dashboard.jsx
@@ -1,9 +1,40 @@
 import Header from "../components/Header";
+import { useState, useEffect } from 'react';
+import { getOrders } from '../services/orders-api';
+import { Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import OrderTable from '../components/OrdersTable';
 
 function Dashboard() {
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+
+        const fetchOrders = async () => {
+            try {
+                const response = await getOrders();
+                setOrders(response.data);
+            } catch (error) {
+                toast.error('Failed to fetch orders.');
+                console.error('Error fetching orders:', error);
+            }
+        };
+        fetchOrders();
+    }, []);
+
     return (
         <>
             <Header />
+            <div className="container mt-4 mb-3">
+                <div className="d-flex justify-content-between align-items-center ">
+                    <h2> Dashboard</h2>
+                    <Link to="/create" className="btn btn-dark">
+                        + Create Order
+                    </Link>
+                </div>
+                <OrderTable orders={orders} />
+
+            </div>
         </>
 
     );
