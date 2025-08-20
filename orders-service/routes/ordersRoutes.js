8559769584e01d8569448108f207432bea8df16f
Shruti Jain <127991092+shruti408@users.noreply.github.com>
2025-08-20 15:19:53 +0530

    Update ordersRoutes.js

--- orders-service/routes/ordersRoutes.js
+++ orders-service/routes/ordersRoutes.js
@@ -7,7 +7,7 @@ const multer = require('multer');
 const upload = multer({ storage: multer.memoryStorage() })
 
 // create new order 
-router.post('/orders', upload.single('invoiceFile'), async (req, res) => {
+router.post('/', upload.single('invoiceFile'), async (req, res) => {
      const { customerName, orderAmount } = req.body;
     const invoiceFile = req.file;
 
@@ -26,7 +26,7 @@ router.post('/orders', upload.single('invoiceFile'), async (req, res) => {
 });
 
 // get all orders 
-router.get('/orders', async (req, res) => {
+router.get('/', async (req, res) => {
     try {
         const orders = await getAllOrders();
         res.status(200).json(orders);
